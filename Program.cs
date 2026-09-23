aa64cf934426becc5d1e7163e62693ecaa4c20a4
trachisan <andy192001d@gmail.com>
2026-09-23 12:43:48 +0300

    ya stvoriv project

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HelloVovaAndAndrii
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello this is my first project on Github mama");
+        }
+    }
+}
